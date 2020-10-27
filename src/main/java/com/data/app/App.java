43c3542fa38f97ab4a7cf084396e2e0bf0148826
kundan <kundan621@gmail.com>
2020-10-26 22:04:55 -0700

    first

---
+++ src/main/java/com/data/app/App.java
@@ -0,0 +1,13 @@
+package com.data.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

daeec35c4b4c4df35d6945f825170ca11d80229a
kundan <kundan621@gmail.com>
2020-10-28 09:19:58 +0530

    test

--- src/test/java/com/kundan/app/AppTest.java
+++ src/test/java/com/kundan/app/AppTest.java
@@ -1,17 +1,14 @@
-/*
- * package com.kundan.app;
- * 
- * import static org.junit.Assert.assertTrue;
- * 
- * import org.junit.Test;
- * 
- *//**
-	 * Unit test for simple App.
-	 */
-/*
- * public class AppTest {
- *//**
-	 * Rigorous Test :-)
-	 *//*
-		 * @Test public void shouldAnswerWithTrue() { assertTrue( true ); } }
-		 */
+
+package com.kundan.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
